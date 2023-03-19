b7aaca4a36155a289766e08cdb1a1916aab5d302
laura-williams-1 <laurawilliams@pursuit.org>
2023-03-19 13:13:09 -0400

    ignore

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,54 +1,54 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import './search.css'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./search.css";
+import { useNavigate } from "react-router-dom";
 
 function Search({ videos }) {
-  const navigate = useNavigate()
-  const [trendingVideo, setTrendingVideo] = useState([])
+  const navigate = useNavigate();
+  const [trendingVideo, setTrendingVideo] = useState([]);
 
   useEffect(() => {
     fetch(
       `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=15&key=${process.env.REACT_APP_API_KEY}`
     )
-      .then(res => res.json())
-      .then(response => {
-        console.log('we made it!')
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("we made it!");
         //DATA ARR
-        let popVid = response.items
-
+        let popVid = response.items;
+        console.log(popVid);
         let trending = popVid.map((video, index) => {
-          console.log(video)
-          let popVidImg = video.snippet.thumbnails.medium.url
+          console.log(video);
+          let popVidImg = video.snippet.thumbnails.medium.url;
           return (
             <li
               key={index}
               onClick={() => {
-                navigate(`/video/${video.id}`)
+                navigate(`/video/${video.id}`);
               }}
             >
-              <img src={popVidImg} alt='trending-video-thumbnail' />
+              <img src={popVidImg} alt="trending-video-thumbnail" />
               <h2>{video.snippet.title}</h2>
             </li>
-          )
-        })
-        console.log(trending)
-        setTrendingVideo(trending)
-      }) //THEN CLOSING TAG
-  }, [])
-  useEffect(() => console.log(trendingVideo), [trendingVideo])
+          );
+        });
+        console.log(trending);
+        setTrendingVideo(trending);
+      }); //THEN CLOSING TAG
+  }, []);
+  useEffect(() => console.log(trendingVideo), [trendingVideo]);
 
   return (
-    <div className='search'>
-      <div className='results'>
-        <ul className='video-results'>
+    <div className="search">
+      <div className="results">
+        <ul className="video-results">
           {videos.length > 0 ? null : trendingVideo}
 
           {videos && videos}
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
